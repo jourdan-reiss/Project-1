d512a1f3898d9996d456b671eccbb326a84ce4e8
Jourdan-Reiss Russell <jourdan.reiss@gmail.com>
2017-02-22 10:58:02 +0000

    New project, committing all existing files

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private Vector3 lastPlayerPosition;
+	private float DistanceToMove;
+
+	public GameObject player;
+
+
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		lastPlayerPosition = player.transform.position; //the initial position at start, and the position on the last frame
+	}
+	
+ 	public void FixedUpdate () 
+	{
+		transform.position = new Vector3 (transform.position.x + DistanceToMove, transform.position.y, transform.position.z);
+
+		DistanceToMove = player.transform.position.x  - lastPlayerPosition.x;
+
+		lastPlayerPosition = player.transform.position;
+	}
+
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rb2D;
+
+
+	void Start () 
+	{
+		rb2D = GetComponent <Rigidbody2D> ();
+	}
+		
+	void FixedUpdate () 
+	{
+		rb2D.AddForce (transform.right * speed);
+		 
+	}
+}
