dd702aef37fdb76d14223a4ca5de05a3aeff6c5e
Jourdan-Reiss Russell <jourdan.reiss@gmail.com>
2017-03-10 00:31:18 +0000

    Lots of headway: -added new prefabs -new wave creation mechanic partway finished, tested with boxes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,41 @@
-﻿using UnityEngine;
+﻿ using UnityEngine;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	private Vector2 pointerPosition;
+	public GameObject wavePrefab;
+	public GameObject thing;
+
+
+	private Wave currentWave;
+
 	
+	void Update () 
+	{
+		pointerPosition = Input.mousePosition;
+
+		if (Input.GetButtonDown ("Fire1"))
+			WavePrepare ();
+		if (Input.GetButton ("Fire1")) 
+		{
+			currentWave.transform.position = WorldPosition ();
+			currentWave.transform.position = new Vector3 (currentWave.transform.position.x, 0f, currentWave.transform.position.z);
+		}
+		if (Input.GetButtonUp ("Fire1"))
+			currentWave.SetSolid ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	Vector3 WorldPosition ()
+	{
+		return Camera.main.ScreenToWorldPoint (new Vector3 (pointerPosition.x,pointerPosition.y , 10f));
+	}
+
+	void WavePrepare ()
+	{
+		
+		GameObject prep = Instantiate (wavePrefab, WorldPosition (), Quaternion.identity) as GameObject;
+		currentWave = prep.GetComponent <Wave> (); 
+		currentWave.SetOutline ();
 	}
 }
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave : MonoBehaviour {
+
+	public GameObject wave;
+	public GameObject outline; 
+
+
+	public void SetOutline ()
+	{
+		wave.SetActive (false);
+		outline.SetActive (true);
+	}
+
+	public void SetSolid ()
+	{
+		wave.SetActive (true);
+		outline.SetActive (false);
+	}
+
+	public void Attach ()
+	{
+	}
+}
